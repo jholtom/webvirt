ebd43df1531e2f9dd7b71ff4b0295e2630440dc0
Peter Foley <pefoley2@verizon.net>
2013-05-05 10:08:34 -0400

    do not send password through shell

--- scripts/adduser.sh
+++ scripts/adduser.sh
@@ -11,10 +11,6 @@ if [ ! -f "$1" ]; then
     exit
 fi
 
-echo -n "New Password: "
-read -s password
-echo
-
-hash=`python2 -c "import bcrypt; print(bcrypt.hashpw('$password', bcrypt.gensalt()))"`
+hash=`python2 -c "import bcrypt,getpass; print(bcrypt.hashpw(getpass.getpass(), bcrypt.gensalt()))"`
 sqlite3 $1 "INSERT INTO users VALUES('$2', '$hash')"
 echo "Done"
